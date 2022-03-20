cf82ee9f1c6890bc40a18f24a3a1a8bd147ed3d7
Mehedi Rana <mdmehedirana@gmail.com>
2022-03-20 12:41:15 +0600

    Stack navigation added

---
+++ src/navigation/stack-navigation/StackNavigation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+
+const Stack = createNativeStackNavigator();
+
+const StackNavigation = () => {
+  return <NavigationContainer>
+      
+  </NavigationContainer>;
+};
