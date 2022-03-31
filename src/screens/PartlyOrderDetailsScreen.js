81f69973ad3b9f1d851e8548450c54e548883d11
Mehedi Rana <mdmehedirana@gmail.com>
2022-03-31 18:22:34 +0600

    Delivery partly order details added

--- src/screens/PartlyOrderDetailsScreen.js
+++ src/screens/PartlyOrderDetailsScreen.js
@@ -1,12 +1,42 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import HeaderText from '../components/header/HeaderText';
+import {COLORS, FONTS} from '../styles/theme';
 
-const PartlyOrderDetailsScreen =()=> {
+const PartlyOrderDetailsScreen = ({navigation}) => {
   return (
-    <View>
-      <Text>PartlyOrderDetailsScreen</Text>
+    <View style={styles.container}>
+      <View style={{marginHorizontal:10}}>
+        <HeaderText
+          headerText="Order Details"
+          headerRight="Rejected"
+          navigation={navigation}
+        />
+      </View>
+
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          marginHorizontal: 22,
+        }}>
+        <Text style={[{color: COLORS.black}, FONTS.bodyMedium]}>
+          Order # GM2D36-51
+        </Text>
+        <Text style={[{color: COLORS.gray}, FONTS.small]}>5 mins ago</Text>
+      </View>
+
+      <View style={{borderBottomWidth:1, borderColor: COLORS.gray, marginTop:18, marginHorizontal:20}}/>
+      <Text style={{marginTop:12, marginLeft:20}}>Items ordered</Text>
     </View>
-  )
-}
+  );
+};
 
 export default PartlyOrderDetailsScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: COLORS.whitePure,
+    flex: 1,
+  },
+});
